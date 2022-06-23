45436e693020249b3647fedf326d64890743b2a1
jumorap <jumorap@unal.edu.co>
2022-06-22 21:08:25 -0500

    Test with hosts added

--- test.sh
+++ test.sh
@@ -9,6 +9,6 @@ for nodes in 1 2 4 8
 do
 echo "Running $img test with $nodes nodes"
 echo -e "\n>> $img test with $nodes nodes" >> results.txt
-mpirun -np ${nodes} ./gray ./Images/${img}.png ./Output/${img}_"${nodes}"nds.png >> results.txt
+mpirun -np ${nodes} --hostfile mpi_hosts ./gray ./Images/${img}.png ./Output/${img}_"${nodes}"nds.png >> results.txt
 done
 done
